f38fdd34f139963f9aa602c06097c211550cc920
Kiryl Alexandrovich <kiryl.alexandrovich@gmail.com>
2018-10-03 21:12:27 +0300

    longest consecutive sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let result = 0;
+  const map = new Map();
+
+  for (const item of array) {
+
+    if (map.has(item)) {
+      continue;
+    }
+
+    const left = map.has(item - 1) ? map.get(item - 1) : item;
+    const right = map.has(item + 1) ? map.get(item + 1) : item;
+
+    if (right - left + 1 > result) {
+      result = right - left + 1;
+    }
+
+    map.set(left, right);
+    if (left !== right) {
+      map.set(right, left);
+    }
+
+    if (left !== item && right !== item) {
+      map.set(item, 0);
+    }
+  }
+
+  return result;
 }
